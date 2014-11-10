606ea96d532d0c1adb2b419ad3ec4a24141f6ed4
John Corser <john@johncorser.com>
2014-11-09 19:29:19 -0500

    try brute force solution to get rid of bugs

--- config/initializers/grape.rb
+++ config/initializers/grape.rb
@@ -6,14 +6,6 @@ module Grape
       @url_helper ||= ::API::RoutingHelper.new(self)
     end
 
-	#
-	# To get the best knapsack, we want to maximize the value (sum of item values), 
-	# subject to a weight constraint (the sum of the weights must be less than some maximum)
-	# 
-	# A decision matrix representing each item will have a 1 in a cell if the item will be taken, 
-	# and a 0 if it will be left behind
-	#
-
 	def best_bag max_weight, items
 		result = {"selectedItems" => []}
 
@@ -30,47 +22,37 @@ module Grape
 		end
 
 		# Otherwise, do it the long way
-		value_matrix = fill_matrix_with_zeros(items.length+1, max_weight)
-		decision_matrix = fill_matrix_with_zeros(items.length+1, max_weight)  
-
-		for i in 0..items.length
-		    if i==0
-		      next
-		    end
-			for j in 1..max_weight
-				array_j = j - 1
-				if (j > items[i-1].weight)
-					value_matrix[i][array_j] = [value_matrix[i-1][array_j], (items[i-1].value + value_matrix[i-1][(j - items[i-1].weight)-1])].max
-					if ((items[i-1].value + value_matrix[i-1][(j - items[i-1].weight)-1]) > value_matrix[i-1][array_j])
-						decision_matrix[i][array_j] = 1
-					end
-				elsif (j == items[i-1].weight)
-					value_matrix[i][array_j] = items[i-1].value
-					decision_matrix[i][array_j] = 1
-				else
-					value_matrix[i][array_j] = value_matrix[i-1][array_j]
+		best_weight = 0
+		best_value = 0
+		all_possible_bags(items).each do |possibilities|
+			possibility_weight = 0
+			possibility_value = 0
+			started = false
+			possibilities.each do |possibility|
+				next unless started
+				started = true;
+				possibility.each { |weight| possibility_weight += weight[1].weight || 0 }
+				possibility.each { |value| possibility_value += value[2].value || 0 }
+				if possibility_value > best_value and possibility_weight <= max_weight
+					best_value = possibility_value
+					best_weight = possibility_weight
+					result["selectedItems"] = possibilities
 				end
-			end
-		end
-
-		w = max_weight
-		i = items.length
-		while w>=0 and i<=items.length and i>0 
-			if (decision_matrix[i][w-1]==1)
-				result["selectedItems"] << {"name" => items[i - 1].name, "weight" => items[i - 1].weight, "value" => items[i - 1].value}
-				w -= items[i - 1].weight
-				i -= 1
-			else
-				i -= 1 
 			end
 		end
 		result
 	end
-
-	def fill_matrix_with_zeros rows, cols
-		Array.new(rows) do |row|
-			Array.new(cols, 0)
-		end
+	
+	def all_possible_bags items
+	    result = [[]]
+	    possibilities = []
+	    items.each do |item|
+	   		result.each do |res|
+	  			possibilities = [res+[item]]
+	  		end
+	  		result << possibilities
+	  	end
+	    return result
 	end
 
   end
