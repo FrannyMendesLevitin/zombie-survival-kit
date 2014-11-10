732ce46e41f6d589f37b53cbc2d2b04743109978
John Corser <john@johncorser.com>
2014-11-10 00:38:07 -0500

    more tests

--- spec/api/post_spec.rb
+++ spec/api/post_spec.rb
@@ -71,4 +71,11 @@ describe Survival::Post do
         expect(response.status).to eq 201
         expect(response.body.gsub(/\s+/, "")).to eq File.read('test_cases/outputs/sample_output_array_contains_non_items.json').to_json.gsub('\\t', '').gsub('\\n', '').gsub(/\s+/, "")
     end
+    it "POST survival-pack" do  
+        file = File.read('test_cases/inputs/sample_input_floats.json')
+        data_hash = JSON.parse(file)
+        post '/v1/survival-pack/pack', data_hash.to_json, 'Content-Type' => 'application/json'
+        expect(response.status).to eq 201
+        expect(response.body.gsub(/\s+/, "")).to eq File.read('test_cases/outputs/sample_output_floats.json').to_json.gsub('\\t', '').gsub('\\n', '').gsub(/\s+/, "")
+    end
 end
