55aa8c8c87dfe586ecabe422fb58b3200aba3da1
John Corser <john@johncorser.com>
2014-11-09 22:21:49 -0500

    passing test cases

--- config/initializers/grape.rb
+++ config/initializers/grape.rb
@@ -6,14 +6,7 @@ module Grape
       @url_helper ||= ::API::RoutingHelper.new(self)
     end
 
-	#
-	# To get the best knapsack, we want to maximize the value (sum of item values), 
-	# subject to a weight constraint (the sum of the weights must be less than some maximum)
-	# 
-	# A decision matrix representing each item will have a 1 in a cell if the item will be taken, 
-	# and a 0 if it will be left behind
-	#
-
+    # Using brute force solution because my dynamic programming attempt was buggy. Might rewrite more scalable version later.
 	def best_bag max_weight, items
 		result = {"selectedItems" => []}
 		maxval = 0
@@ -21,17 +14,14 @@ module Grape
 		bags = all_possible_bags(items)
 
 		bags.each do |possibile_bag|
+			next if possibile_bag == []
 			value = 0
 			weight = 0
-			possibile_bag.each do |bag|
-				bag.flatten!
-				bag.each do |item|
-					weight += item.weight
-					value += item.value
-				end
+			possibile_bag.each do |item|
+				weight += item.weight 
+				value += item.value 
 			end
 			next if weight > max_weight
-
 			if value == maxval
 				solutions << possibile_bag
 			elsif value > maxval
@@ -39,22 +29,26 @@ module Grape
 				solutions = possibile_bag
 			end
 		end
-		solutions.each do |set|
-			set.each {|item| result["selectedItems"] << {"name" => item.name, "weight" => item.weight, "value" => item.value}}
+		solutions.each do |item|
+			result["selectedItems"] << {"name" => item.name, "weight" => item.weight, "value" => item.value}
 		end
 		result
 	end
 
+	# Adapted from Powerset code found on StackOverflow
 	def all_possible_bags items
-	    result = []
-	    possibilities = []
-	    items.each do |item|
-	        result.each do |res|
-	            possibilities = [res+[item]]
-	        end
-	        result << possibilities
-	    end
-	    result
+		yield [] if block_given?
+		items.inject([[]]) do |ps, elem|
+			r = []
+			ps.each do |i|
+				r << i
+				new_subset = i + [elem]
+				yield new_subset if block_given?
+				r << new_subset
+			end
+			r
+		end
 	end
+
   end
 end
